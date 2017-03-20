2030569cd4f9f34bc77c5c1d2c7bf359b6910b40
Sourabh <sourabh.jamalapuram@gmail.com>
2017-03-19 19:23:47 -0700

    Initial check in for report desk app

---
+++ 2017/server/ios/reportdeskapp/reportdeskapp/Program.swift
@@ -0,0 +1,18 @@
+//
+//  Program.swift
+//  reportdeskapp
+//
+//  Created by Sourabh Jamlapuram on 3/19/17.
+//  Copyright © 2017 gpta. All rights reserved.
+//
+
+import Foundation
+
+class Program{
+    var name: String!
+    
+    init(name:String ){
+        self.name = name
+
+    }
+}
