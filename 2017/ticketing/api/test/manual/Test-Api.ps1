e3230fc5f29345feda79f958709335a7b5121e0d
sairama <sairamaj@hotmail.com>
2017-04-04 16:48:08 -0700

    checkins GET/POST were added

---
+++ 2017/ticketing/api/test/manual/Test-Api.ps1
@@ -0,0 +1,7 @@
+﻿# post checkin
+$postData = @"
+{ "id": "A12", "adults" : "1", "kids" : "2"}
+"@
+
+Invoke-RestMethod -Method Post -Uri https://parfmou7ta.execute-api.us-west-2.amazonaws.com/Prod/tickets/checkins -Body $postData
+Invoke-RestMethod -Method Get -Uri https://parfmou7ta.execute-api.us-west-2.amazonaws.com/Prod/tickets/checkins
---
+++ 2017/ticketing/api/test/manual/Test-LocalApi.ps1
@@ -0,0 +1,7 @@
+﻿# post checkin
+$postData = @"
+{ "id": "1245", "adults" : "1", "kids" : "2"}
+"@
+
+Invoke-RestMethod -Method Post -Uri http://localhost:4000/tickets/checkins -Body $postData
+Invoke-RestMethod -Method Get -Uri http://localhost:4000/tickets/checkins
