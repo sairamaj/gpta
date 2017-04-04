ea67bfcf5238a34c602f7ac125527278c8edbcd4
sairama <sairamaj@hotmail.com>
2017-04-03 18:15:56 -0700

    moving directories

---
+++ 2017/ticketing/api/ticketapp/ticketapp/Model/TicketHolderArrivalStatus.swift
@@ -0,0 +1,16 @@
+//
+//  TicketHolderArrivalStatus.swift
+//  ticketapp
+//
+//  Created by Sourabh Jamlapuram on 4/2/17.
+//  Copyright © 2017 gpta. All rights reserved.
+//
+
+import Foundation
+
+enum TicketHolderArrivalStatus
+{
+    case noneArrived
+    case partialArrived
+    case allArrived
+}
