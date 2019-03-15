ede2ba179c9b16bfa571207903d748e76e6af81e
Sourabh <sourabh.jamalapuram@gmail.com>
2019-03-14 21:54:48 -0700

    Shell scripts added

--- 2019/ticketing/api/localdb/createTables.bat
+++ 2019/ticketing/api/localdb/createTables.bat
@@ -1,3 +1,3 @@
 aws dynamodb create-table --table-name Ticket --attribute-definitions AttributeName=id,AttributeType=S AttributeName=name,AttributeType=S --key-schema AttributeName=id,KeyType=HASH AttributeName=name,KeyType=RANGE --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1 --endpoint-url http://192.168.56.1:8000
 
-rem aws dynamodb create-table --table-name TicketCheckIn --attribute-definitions AttributeName=id,AttributeType=S --key-schema AttributeName=id,KeyType=HASH --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1 --endpoint-url http://172.17.0.2:8000
+aws dynamodb create-table --table-name TicketCheckIn --attribute-definitions AttributeName=id,AttributeType=S --key-schema AttributeName=id,KeyType=HASH --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1 --endpoint-url http://172.17.0.2:8000
--- 2019/ticketing/api/localdb/listData.sh
+++ 2019/ticketing/api/localdb/listData.sh
@@ -1,2 +1,3 @@
 aws dynamodb scan --table-name Ticket  --endpoint-url http://127.0.0.1:8000
 aws dynamodb scan --table-name TicketCheckIn  --endpoint-url http://127.0.0.1:8000
+
