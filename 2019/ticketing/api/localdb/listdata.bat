5a134491363593ab658c7f2afd48b8a07013584a
sairamaj <sairamaj@gmail.com>
2019-03-15 19:35:09 -0700

    updated windows batch files

--- 2019/ticketing/api/localdb/listdata.bat
+++ 2019/ticketing/api/localdb/listdata.bat
@@ -1,2 +1,2 @@
-aws dynamodb scan --table-name Ticket  --endpoint-url http://127.0.0.1:8000
-aws dynamodb scan --table-name TicketCheckIn  --endpoint-url http://127.0.0.1:8000
+aws dynamodb scan --table-name Ticket  --endpoint-url http://192.168.99.100:8000
+aws dynamodb scan --table-name TicketCheckIn  --endpoint-url http://192.168.99.100:8000
