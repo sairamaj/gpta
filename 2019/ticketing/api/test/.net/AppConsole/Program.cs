87a6a7e30c27f58585ee8372b1d803667ab0a2d1
sairamaj <sairamaj@gmail.com>
2019-03-17 15:31:54 -0700

    Adding .NET test console

---
+++ 2019/ticketing/api/test/.net/AppConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Pathoschild.Http.Client;
+
+namespace AppConsole
+{
+    class Ticket{
+        public String Name {get; set;}
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var client = new FluentClient("http://localhost:4000");
+            var tickets = client.GetAsync("/tickets").As<IEnumerable<Ticket>>().Result;
+            foreach(var ticket in tickets){
+                System.Console.WriteLine(ticket.Name);
+            }
+        }
+    }
+}
