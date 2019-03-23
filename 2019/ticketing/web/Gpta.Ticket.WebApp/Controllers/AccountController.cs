6b779dd468f5c289283dd5cb802209f99810f372
sairamaj <sairamaj@gmail.com>
2019-03-23 08:59:28 -0700

    access denied page added

---
+++ 2019/ticketing/web/Gpta.Ticket.WebApp/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Gpta.Ticket.WebApp.Models;
+
+namespace Gpta.Ticket.WebApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult AccessDenied()
+        {
+            return View(User.Claims);
+        }
+    }
+}
